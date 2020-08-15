e3501b82784dee768571e5e45c94b01c6f626387
Hossam Mourad <hossam.mourad@fullscript.com>
2020-08-15 07:15:29 -0400

    display list

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Fragment } from "react";
+import { StyleSheet, View, ScrollView, StatusBar as StatusBarNative } from "react-native";
+// @ts-ignore
+import { Text } from "galio-framework";
+import data from "./album-list.json";
 
 export default function App() {
+  const renderAlbums = () => {
+    return data.map((single) => {
+      return (
+        <Fragment key={single.Number}>
+          <Text p style={styles.text}>
+            {single.Number} - {single.Album}
+          </Text>
+        </Fragment>
+      );
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        <StatusBar hidden />
+        {renderAlbums()}
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+  },
+
+  scrollView: {
+    padding: StatusBarNative.currentHeight,
+  },
+
+  text: {
+    color: "#FFF",
   },
 });
